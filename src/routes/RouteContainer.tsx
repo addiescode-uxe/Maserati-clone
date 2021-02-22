644f0b203730d0611afddff5778e94e42ff95275
ahnytae <ahnheetae@ucomp.co.kr>
2021-02-22 23:38:55 +0900

    :lipstick: NAV 구조 셋팅 및 기본메뉴 작성
    nav 기본메뉴

--- src/routes/RouteContainer.tsx
+++ src/routes/RouteContainer.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { Switch, Route, Redirect, useLocation } from 'react-router-dom';
+import NavContainer from '../components/Nav/index';
 import { routes } from './index';
 
 const RouteContainer: React.FC = () => {
@@ -10,15 +11,18 @@ const RouteContainer: React.FC = () => {
   }, [pathname]);
 
   return (
-    <Switch>
-      <Redirect exact path="/" to="/home" />
-      {routes.map(({ path, component: Component, exact = true }, index) => (
-        <Route exact={exact} path={path} key={index}>
-          <Component />
-        </Route>
-      ))}
-      <Redirect from="*" to="/" />
-    </Switch>
+    <>
+      <NavContainer />
+      <Switch>
+        <Redirect exact path="/" to="/home" />
+        {routes.map(({ path, component: Component, exact = true }, index) => (
+          <Route exact={exact} path={path} key={index}>
+            <Component />
+          </Route>
+        ))}
+        <Redirect from="*" to="/" />
+      </Switch>
+    </>
   );
 };
 export default RouteContainer;
